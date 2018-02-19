8f2e66fb8177b2dc4b2c64b315cb8bfa6a408179
Pravin Tripathi <pravin.tripathi@ves.ac.in>
2018-02-19 13:34:44 +0530

    few modification done

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+
+$Fname = $_POST["fname"];
+$Lname = $_POST["lname"];
+$age = $_POST["age"];
+$image_data = $_POST["image_data"];
+$college = $_POST["college"];
+$stream = $_POST["stream"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+
+?>
