84a27eb57a459d75ecfa360bed3e180e9a98575c
Pravin Tripathi <pravin.tripathi@ves.ac.in>
2018-02-23 01:11:46 +0530

    control stage one done

--- backend_analysis/driver.php
+++ backend_analysis/driver.php
@@ -1,19 +1,13 @@
 <?php
-    $cmd = "ping 127.0.0.1";
-
-    $descriptorspec = array(
-       0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
-       1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
-       2 => array("pipe", "w")    // stderr is a pipe that the child will write to
-    );
-    flush();
-    $process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
-    echo "<pre>";
-    if (is_resource($process)) {
-        while ($s = fgets($pipes[1])) {
-            print $s;
-            flush();
-        }
+    $python = "c:\\python36\\python.exe";
+    if($_GET["id"]==2){
+        $file = "C:\\xampp\htdocs\\pb\\front-end\\backend_analysis\\stop.py";
+        $output=exec($python . " " . $file);
+        echo $output;
+    }else if($_GET["id"]==1){
+        $file = "C:\\xampp\htdocs\\pb\\front-end\\backend_analysis\\start.py";
+        $output=exec($python . " " . $file);
+        echo $output;
     }
-    echo "</pre>";
+    
 ?>
--- backend_analysis/start.py
+++ backend_analysis/start.py
@@ -7,11 +7,13 @@ def start(file):
         fwrite.write("================== Process Started ==================\n")
     
     if file.exists():
+        ## write our code for video taking
         while(file.exists()):
             with open(file,'a') as fwrite:
                 fwrite.write("Access:" + time.ctime())
     else:
         print("No file")
 
+#log file is the heart
 file=Path("log.txt")
 start(file)
